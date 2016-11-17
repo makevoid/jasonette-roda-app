648f442ddc12b2a3e78079abc40913f441449b59
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-11-17 01:27:28 +0000

    Update camera.rb
    note - not finished

--- sample_apps/camera.rb
+++ sample_apps/camera.rb
@@ -1,3 +1,6 @@
+# TODO: not finished - check https://github.com/makevoid/jasonette-roda-app/blob/master/sample_apps/weather.rb for the weather example (weather.rb in sample_apps)
+
+
 AppTitle = "Hello World"
 
 Camera = {
